47ce5b441b2b0c33690cf98054d553aba5caf82e
Matt Windsor <mattwindsor91@gmail.com>
2022-12-31 23:04:00 +0000

    Start adding CLI infrastructure
    This is just a single subcommand, for now.  It doesn't really do anything.

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,44 @@
 
 #![warn(clippy::pedantic)]
 
-use std::{path::Path, process::exit};
+use std::{
+    path::{Path, PathBuf},
+    process::exit,
+};
 
-pub use pest::Parser;
+use clap::{Parser, Subcommand};
+
+/// Automated concurrent algorithm proof checker (second edition).
+#[derive(clap::Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+/// Subcommands for the `starling` CLI.
+#[derive(Subcommand)]
+enum Commands {
+    /// Performs basic semantic analysis on a PVC script.
+    Lint {
+        /// Path to the PVC script to analyse.
+        #[arg(required = true)]
+        path: PathBuf,
+    },
+}
 
 fn main() -> eyre::Result<()> {
     color_eyre::install()?;
 
-    let path = "example.sta";
+    let args = Cli::parse();
+    let (cur_path, err) = match args.command {
+        Commands::Lint { path } => (path.to_string_lossy().into_owned(), lint(&path)),
+    };
 
-    match run(path) {
+    match err {
         Err(Error::Parser(starling::parser::Error::Parse(e))) => {
-            eprintln!("Parse error in file {path}:");
+            eprintln!("Parse error in file {cur_path}:");
             eprintln!("{e}");
             exit(1);
         }
@@ -23,7 +49,7 @@ fn main() -> eyre::Result<()> {
     Ok(())
 }
 
-fn run(path: impl AsRef<Path>) -> Result<()> {
+fn lint(path: impl AsRef<Path>) -> Result<()> {
     let contents = std::fs::read_to_string(path)?;
     let ast = starling::parser::parse(&contents)?;
 
